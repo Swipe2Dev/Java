aca203fd553e616c2d98f69ae4bfa4eda821140b
Kausalya-s673 <164292535+Kausalya-s673@users.noreply.github.com>
2026-05-11 10:09:58 +0530

    Student intro file created

---
+++ src/main/java/StudentIntro.java
@@ -0,0 +1,18 @@
+public class StudentIntro {
+
+    public static void main(String[] args){
+
+        String name="Rahul";
+        int age=22;
+        String city="Bangalore";
+        double percentage=85.4;
+        boolean graduated=true;
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("City: "+city);
+        System.out.println("Percentage: "+percentage);
+        System.out.println("Graduated: "+graduated);
+
+    }
+}
